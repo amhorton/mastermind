7412e254c1b740468ef6bcde88728cc6ff96e57f
amhorton <aaronmichaelhorton@gmail.com>
2014-04-07 01:09:42 -0500

    Update mastermind_tester.rb

--- mastermind_tester.rb
+++ mastermind_tester.rb
@@ -1,4 +1,4 @@
-class Fixnum
+class Fixnum #the program is the same, except is plays against itself a set number of times and displays how many times it's failed to guess the code in 10 tries.
     def invalid?
     	selfstring = self.to_s
         if selfstring.include?('0')
@@ -13,109 +13,90 @@ class Fixnum
             return false
         end
     end
+    
     def eliminated?(last_guess, user_response)
-		if compare(self, last_guess) != user_response
-			return true
-		else
-			return false
-		end
+        if compare(self, last_guess) != user_response
+	    return true
+	else
+	    return false
 	end
-	def translate
-		guess_split = self.to_s.split(//)
-		guess_split.collect {|n|
-			if n == "1"
-				n = "Red"
-			elsif n == "2"
-				n = "Orange"
-			elsif n == "3"
-				n = "Yellow"
-			elsif n == "4"
-				n = "Green"
-			elsif n == "5"
-				n = "Blue"
-			elsif n == "6"
-				n = "Purple"
-			end
-		}
-	end
+    end
+    
+    def translate
+	guess_split = self.to_s.split(//)
+	guess_split.collect {|n|
+	    if n == "1"
+		n = "Red"
+	    elsif n == "2"
+		n = "Orange"
+	    elsif n == "3"
+		n = "Yellow"
+	    elsif n == "4"
+		n = "Green"
+	    elsif n == "5"
+		n = "Blue"
+	    elsif n == "6"
+		n = "Purple"
+	    end
+	}
+    end
 end
 
 def compare(guess, solution)
-
-	guess_split = guess.to_s.split(//)
-	solution_split = solution.to_s.split(//)
-	black_peg = 0
-	white_peg = 0
-	
-	for i in 0..3
-		if guess_split[i] == solution_split[i]
-			black_peg += 1
-			solution_split[i] = "ALSO COUNTED"
-			guess_split[i] = "COUNTED"
-		end
+    guess_split = guess.to_s.split(//)
+    solution_split = solution.to_s.split(//)
+    black_peg = 0
+    white_peg = 0
+    for i in 0..3
+        if guess_split[i] == solution_split[i]
+	    black_peg += 1
+	    solution_split[i] = "ALSO COUNTED"
+	    guess_split[i] = "COUNTED"
 	end
-	for i in 0..3
-		if guess_split.include?(solution_split[i]) == true
-			white_peg += 1
-			guess_split.delete_at(guess_split.find_index(solution_split[i]))
-		end
+    end
+    for i in 0..3
+	if guess_split.include?(solution_split[i]) == true
+	    white_peg += 1
+	    guess_split.delete_at(guess_split.find_index(solution_split[i]))
 	end
-	return[black_peg, white_peg]
+    end
+    return[black_peg, white_peg]
 end
 
 set = Array.new
 
 for i in 1111..6666 do
-	if i.invalid? != true
-		set << i
-	end
-end
-
-def make_a_guess(guess)
-	puts "Computer's guess: #{guess.translate}"
-	puts "How many black pegs? This should be a number between 0 and 4"
-	black_peg = gets.chomp.to_i
-	if black_peg > 4 or black_peg < 0
-		puts "error! That's not a number between 0 and 4."
-		return "redo"
-	end
-	puts "How many white pegs? This should be a number between 0 and 4"
-	white_peg = gets.chomp.to_i
-	if white_peg > 4 or white_peg < 0
-		puts "error! That's not a number between 0 and 4."
-		return "redo"
-	end
-	return [black_peg, white_peg]
+    if i.invalid? != true
+	set << i
+    end
 end
 
 user_response = []
 last_guess = 1122
 tries = 0
 failures = 0
-set_clone = set
-total_tries = 0
-until total_tries == 1000 do
-	while user_response != [4, 0] do
-		secret_code = set_clone.shuffle.first
-		user_response = compare(last_guess, secret_code)
-		set.reject! {|n| n.eliminated?(last_guess, user_response) == true}
-		last_guess = set.shuffle.first
-		tries += 1
+total_games = 0
+until total_games == 1000 do 
+    secret_code = set_clone.shuffle.first #generates a new secret code for the computer to guess
+    while user_response != [4, 0] do
+	user_response = compare(last_guess, secret_code) #mimics the user response using "compare"
+	set.reject! {|n| n.eliminated?(last_guess, user_response) == true}
+	last_guess = set.shuffle.first
+	tries += 1
+    end
+    if tries > 10 
+	failures += 1
+    end
+    for i in 1111..6666 do #resetting values and rebuilding the set before starting a new game
+        if i.invalid? != true
+	    set << i
 	end
-	if tries > 10
-		failures += 1
-	end
-	tries = 0
-	user_response = []
-	last_guess = 1122
-	for i in 1111..6666 do
-		if i.invalid? != true
-			set << i
-		end
-	end
-	
-	total_tries += 1
+    end
+    user_response = []
+    last_guess = 1122
+    tries = 0 
+    total_games += 1
 end
-puts total_tries
-puts failures
+puts "Total games: #{total_games}"
+puts "Failures: #{failures}"
 
